be56b039a0956b35f857244594f745b3e043a958
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-12-06 10:01:53 -0800

    Carousel is changing images correctly left and right

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,79 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselContainer = document.querySelector('.carousel-container');
+
+let currentIndex = 1;
+
+function CreateCarousel (){
+  const carousel = document.createElement('div'),
+    leftButton = document.createElement('div'),
+    img1 = document.createElement('img'),
+    img2 = document.createElement('img'),
+    img3 = document.createElement('img'),
+    img4 = document.createElement('img'),
+    rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  img1.classList.add('img-1');
+  img2.classList.add('img-2');
+  img3.classList.add('img-3');
+  img4.classList.add('img-4');
+
+  img1.style.display = "block";
+
+  leftButton.textContent = '<';
+  rightButton.textContent = '>';
+  img1.src = "./assets/carousel/mountains.jpeg";
+  img2.src = "./assets/carousel/computer.jpeg";
+  img3.src = "./assets/carousel/trees.jpeg";
+  img4.src = "./assets/carousel/turntable.jpeg";
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(img1);
+  carousel.appendChild(img2);
+  carousel.appendChild(img3);
+  carousel.appendChild(img4);
+  carousel.appendChild(rightButton);
+
+  rightButton.addEventListener('click', function(){
+    if(currentIndex !== 4){
+      let currentImage = document.querySelector(`.img-${currentIndex}`);
+      currentImage.style.display = "none";
+      currentIndex++;
+      let nextImage = document.querySelector(`.img-${currentIndex}`);
+      nextImage.style.display = "block";
+    } else {
+      let currentImage = document.querySelector(`.img-${currentIndex}`);
+      currentImage.style.display = "none";
+      currentIndex = 1;
+      let nextImage = document.querySelector(`.img-${currentIndex}`);
+      nextImage.style.display = "block";
+    }
+  })
+
+  leftButton.addEventListener('click', function(){
+    if(currentIndex !== 1){
+      let currentImage = document.querySelector(`.img-${currentIndex}`);
+      currentImage.style.display = "none";
+      currentIndex--;
+      let nextImage = document.querySelector(`.img-${currentIndex}`);
+      nextImage.style.display = "block";
+    } else {
+      let currentImage = document.querySelector(`.img-${currentIndex}`);
+      currentImage.style.display = "none";
+      currentIndex = 4;
+      let nextImage = document.querySelector(`.img-${currentIndex}`);
+      nextImage.style.display = "block";
+    }
+  })
+
+  return carousel;
+}
+
+carouselContainer.appendChild(CreateCarousel());
+
